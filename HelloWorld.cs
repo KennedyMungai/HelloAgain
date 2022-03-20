10a049d3a2855ba3a0795776209a6e3311c4dc7c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-03-20 21:44:21 +0300

    Added a nonsensical method

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,6 +6,12 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        WriteLine("Hello World");
+        // WriteLine("Hello World");
+        Crying(1, "over you");
+    }
+
+    static void Crying(int eye, string tears)
+    {
+        WriteLine($"{eye} has caused this much {tears}");
     }
 }
